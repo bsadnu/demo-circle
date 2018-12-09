002c53155da0ef5f84534463d18637c7f6996b7f
Stas Bannikov <sab.ukraine@gmail.com>
2018-12-09 12:33:42 +0000

    8th

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -4,7 +4,7 @@ TOKEN=7383af091845d203a4d0beebe2bd1e4ecdb90a4e
 
 curl --user $TOKEN: \
     --request POST \
-    --form revision=c8f185db196f739e8812caac8dcb196b2a93f557 \
+    --form revision=5d7f45612221fb20fdae6b97f41f3cf3605be16e \
     --form config=@config.yml \
     --form notify=false \
     https://circleci.com/api/v1.1/project/github/bsadnu/demo-circle/tree/master
